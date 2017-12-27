cad856c9c945d0ab894ce4b018514fb58906b3b4
lkx <admin@qq.com>
2017-12-27 15:04:09 +0800

    '11'

--- 1.php
+++ 1.php
@@ -2,3 +2,15 @@
 这是在hotfix上的编辑内容
 这是在hotfix分支上修复的bug
 ---hotfix11.56
+<?php
+
+class a {
+    const APP=1;
+    const PLATEFORM=2;
+    public static function getType(){
+        return [
+                'app'=>self::APP,
+                'plateform'=>self::PLATEFORM
+        ];
+    }
+}
