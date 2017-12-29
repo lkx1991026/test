a3e92172d29433ecad007621986f992373594ad6
lkx <admin@qq.com>
2017-12-29 14:20:06 +0800

    'test'

--- 1.php
+++ 1.php
@@ -1,8 +1,12 @@
 <?php
+if(isset($_FILES['image'])){
+    $image=$_FILES['image'];
+    $path='./'.uniqid().'.'.substr($image['type'],strrpos($image['type'],'/')+1);
+    move_uploaded_file($image['tmp_name'],$path);
     $zip=new ZipArchive();
     $filename='test.zip';
     var_dump($zip->open($filename,ZipArchive::OVERWRITE|ZipArchive::CREATE));
-    $zip->addFile('jquery10.js');
+    $zip->addFile($path);
     $zip->close();
     header ( "Cache-Control: max-age=0" );
     header ( "Content-Description: File Transfer" );
@@ -11,3 +15,11 @@
     header ( "Content-Transfer-Encoding: binary" ); // 告诉浏览器，这是二进制文件
 //    header ( 'Content-Length: ' . filesize ( $filename ) ); // 告诉浏览器，文件大小
     @readfile ( $filename );
+}
+
+    ?>
+<form method="post" action="" enctype="multipart/form-data">
+    <input type="file" name="image"><br/>
+
+    <input type="submit" value="XXX">
+</form>
